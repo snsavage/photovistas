04056ed04da19f9b87f7c4442afd45c21a30bdbd
Scott Savage <snsavage@gmail.com>
2016-11-23 13:34:45 -0500

    Setup initial working web application

---
+++ app/controllers/application.rb
@@ -0,0 +1,8 @@
+require 'sinatra/base'
+
+class Application < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+end
+
---
+++ config.ru
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+
+require_rel "app/"
+
+run Application
