4624c8a31b2d21f6f836258b67d9f3cf57904704
Scott Savage <snsavage@gmail.com>
2016-11-30 21:44:58 -0500

    Add missed change from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,6 @@
 require_relative 'application_controller'
 
 class UsersController < ApplicationController
-  get '/login' do
-    if !logged_in?
-      erb :'/users/login'
-    else
-      redirect to "/"
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect to "/login"
-    else
-      redirect to "/"
-    end
-  end
-
-  post '/login' do
-    redirect to "/" if logged_in?
-
-    user = User.find_by(username: params[:username])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/users/#{user.id}"
-    else
-      flash[:login_errors] = "Please provide a valid username and password."
-      erb :"/users/login"
-    end
-  end
-
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
 
