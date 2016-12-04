f251518af54ce19f320229bd97dddf275aea1c9d
Scott Savage <snsavage@gmail.com>
2016-12-04 14:56:34 -0500

    Create unsplash controller and move routes from signups controller

--- config.ru
+++ config.ru
@@ -3,5 +3,6 @@ require_relative "./config/environment"
 use SessionsController
 use SignUpsController
 use UsersController
+use UnsplashController
 run ApplicationController
 
--- spec/controllers/sign_ups_controller_spec.rb
+++ spec/controllers/sign_ups_controller_spec.rb
@@ -72,28 +72,4 @@ describe SignUpsController do
   end
 end
 
-describe 'unsplash access token' do
-  it 'regenerates a token with access to current user' do
-    # skip "Only use when testing extract process"
-    unsplash_token_setup
-    expect(Unsplash::User.current[:username]).to eq("snsavage")
-  end
-end
-
-describe 'GET /unsplash/unlink' do
-  it 'removes unsplash token and username from db' do
-    user = create(:user)
-    user.unsplash_token = "temp"
-    user.unsplash_username = "temp"
-    user.save!
-
-    get '/unsplash/unlink', {},
-      'rack.session' => {user_id: user.id}
-
-    user = User.find(user.id)
-
-    expect(user.unsplash_token).to be nil
-    expect(user.unsplash_username).to be nil
-  end
-end
 
